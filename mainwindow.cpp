a521010de6c79600b78e666b044f21306d4f723e
orsosa <oj.5070@gmail.com>
2016-10-26 17:43:29 -0300

    graph style changed.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,7 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    this->window()->setWindowTitle("SAMAHO");
+    this->window()->setWindowTitle("HuLC");
     onlineFlag = false;
     offlineTimeout = 70;
     setPlotStyle();
@@ -17,6 +17,7 @@ MainWindow::MainWindow(QWidget *parent) :
     //db.setPort(9999);
     //db.setPassword("");
     if (!db.open()) qDebug()<<db.lastError();
+    query = new  QSqlQuery();
     ui->wifi_frame->setStyleSheet(\
         "background-color: transparent;\
          background-image: url(\"wifi_off.png\");\
@@ -28,14 +29,18 @@ MainWindow::MainWindow(QWidget *parent) :
     y_data_m = new QVector<double>(npoints-avg_w);
     /////// getting histogram limits
     setLimits();
-    ui->gFreqTime->graph()->setPen(QPen(Qt::blue, 1, Qt::DashLine, Qt::RoundCap, Qt::RoundJoin));
-    ui->gFreqTime->graph()->setLineStyle(QCPGraph::lsLine);
-    ui->gFreqTime->graph()->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssDisc, 7));
+    //ui->gFreqTime->graph()->setPen(QPen(Qt::blue, 1, Qt::DashLine, Qt::RoundCap, Qt::RoundJoin));
+    //ui->gFreqTime->graph()->setLineStyle(QCPGraph::lsLine);
+    //ui->gFreqTime->graph()->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssDisc, 7));
+    
+
+
     //////
     connect(ui->gFreqTime->xAxis, SIGNAL(rangeChanged(QCPRange)), ui->gFreqTime->xAxis2, SLOT(setRange(QCPRange)));
     connect(ui->gFreqTime->yAxis, SIGNAL(rangeChanged(QCPRange)), ui->gFreqTime->yAxis2, SLOT(setRange(QCPRange)));
     ui->gFreqTime->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);
     ///////
+
     QGraphicsDropShadowEffect* effect = new QGraphicsDropShadowEffect();
     effect->setBlurRadius(5);
     ui->gFreqTime->setGraphicsEffect(effect);
@@ -54,10 +59,10 @@ void MainWindow::initLimits()
 
 void MainWindow::getTelescopeData()
 {
-    QSqlQuery query;
-    query.exec(QString("SELECT freq, reg_date FROM usm_telescope_data WHERE reg_date >= DATE_SUB(CURRENT_DATE, INTERVAL 6 MONTH) ORDER BY reg_date DESC LIMIT %1").arg(npoints));
-    query.next();
-    if (t_max < query.value(1).toDateTime().toTime_t()) t_max =query.value(1).toDateTime().toTime_t();
+  //    QSqlQuery query;
+    query->exec(QString("SELECT freq, reg_date FROM usm_telescope_data WHERE reg_date >= DATE_SUB(CURRENT_DATE, INTERVAL 6 MONTH) ORDER BY reg_date DESC LIMIT %1").arg(npoints));
+    query->next();
+    /*    if (t_max < query->value(1).toDateTime().toTime_t()) t_max =query->value(1).toDateTime().toTime_t();
     uint t_min_a;
     double f_min_a=1e6;
     while (query.next())
@@ -67,6 +72,7 @@ void MainWindow::getTelescopeData()
     }
 
      query->next();
+    */
      if (t_max < query->value(1).toDateTime().toTime_t()) t_max =query->value(1).toDateTime().toTime_t();
      int i=0;
      double freq = query->value(0).toDouble();
@@ -114,14 +120,32 @@ void MainWindow::setLimits()
         f_min_r>f_min ||
         f_max_r>f_max)
     {
-    ui->gFreqTime->xAxis->setRange(t_min_r,t_max_r);
-    ui->gFreqTime->yAxis->setRange(f_min_r,f_max_r);
+      t_min_r = t_min-0.1*(t_max-t_min);
+      t_max_r = t_max+0.1*(t_max-t_min);
+      f_min_r = f_min-0.1*(f_max-f_min);
+      f_max_r = f_max+0.1*(f_max-f_min);
+      ui->gFreqTime->xAxis->setRange(t_min_r,t_max_r);
+      ui->gFreqTime->yAxis->setRange(f_min_r,f_max_r);
     }
 }
 
 void MainWindow::setPlotStyle()
 {
     ui->gFreqTime->addGraph();
+    ui->gFreqTime->graph()->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, QPen(Qt::black, 1.5), QBrush(Qt::white), 9));
+    ui->gFreqTime->graph()->setPen(QPen(QColor(120, 120, 120), 2, Qt::DashLine));
+
+    QLinearGradient plotGradient;
+    plotGradient.setStart(0, 0);
+    plotGradient.setFinalStop(0, 350);
+    plotGradient.setColorAt(0, QColor(80, 80, 80));
+    plotGradient.setColorAt(1, QColor(50, 50, 50));
+    ui->gFreqTime->setBackground(plotGradient);
+    ui->gFreqTime->xAxis->setTickLabelColor(Qt::white);
+    ui->gFreqTime->yAxis->setTickLabelColor(Qt::white);
+    ui->gFreqTime->xAxis->setLabelColor(Qt::white);
+    ui->gFreqTime->yAxis->setLabelColor(Qt::white);
+
     ui->gFreqTime->xAxis->setTickLabelType(QCPAxis::ltDateTime);
     ui->gFreqTime->xAxis->setDateTimeFormat("mm-ss");
     ui->gFreqTime->xAxis->setLabel("Time (mm-ss)");
@@ -131,11 +155,11 @@ void MainWindow::reDrawFreq()
 {
     getTelescopeData();
     setLimits();
-    query.exec(QString("SELECT freq, reg_date FROM usm_telescope_data WHERE reg_date >= DATE_SUB(CURRENT_DATE, INTERVAL 2 MONTH) ORDER BY reg_date DESC LIMIT %1").arg(npoints));
+    query->exec(QString("SELECT freq, reg_date FROM usm_telescope_data WHERE reg_date >= DATE_SUB(CURRENT_DATE, INTERVAL 2 MONTH) ORDER BY reg_date DESC LIMIT %1").arg(npoints));
     int i=0;
-    while (query.next()) {
-            double freq = query.value(0).toDouble();
-            uint date = query.value(1).toDateTime().toTime_t();
+    while (query->next()) {
+            double freq = query->value(0).toDouble();
+            uint date = query->value(1).toDateTime().toTime_t();
             //qDebug() << freq<< date<<QDateTime::currentDateTime().toTime_t();
             (*x_data)[i] = date;
             (*y_data)[i++] =freq;
@@ -153,8 +177,8 @@ MainWindow::~MainWindow()
 void MainWindow::npoints_valueChanged(int arg1)
 {
     if (arg1<=npoints){initLimits();}
-    if (arg1>50)ui->gFreqTime->graph()->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssDisc, 3));
-    if (arg1<20)ui->gFreqTime->graph()->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssDisc, 7));
+    if (arg1>50) ui->gFreqTime->graph()->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, QPen(Qt::black, 1.5), QBrush(Qt::white), 5));
+    if (arg1<20) ui->gFreqTime->graph()->setScatterStyle(QCPScatterStyle(QCPScatterStyle::ssCircle, QPen(Qt::black, 1.5), QBrush(Qt::white), 9));
     npoints=arg1;
     delete x_data;
     delete y_data;
